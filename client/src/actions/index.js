cb051b6dea5ffa4b65c11ae2fce8d4a8307a6ade
stevenvaleri <steven.valeri@ey.com>
2019-09-07 17:49:29 -0400

    fully working millionaire

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,3 +1,5 @@
+
+// Redux action for when enigma-js client library has been initialized
 export const initializeEnigma = (enigma) => {
     return {
         type: 'ENIGMA_INITIALIZED',
@@ -5,6 +7,7 @@ export const initializeEnigma = (enigma) => {
     };
 };
 
+// Redux action for when web3 accounts have been initialized
 export const initializeAccounts = (accounts) => {
     return {
         type: 'ACCOUNTS_INITIALIZED',
@@ -12,6 +15,23 @@ export const initializeAccounts = (accounts) => {
     };
 };
 
+// Redux action for when millionaires problem has been deployed to a particular address
+export const deployMillionairesProblem = (deployedMillionairesProblem) => {
+    return {
+        type: 'MILLIONAIRES_PROBLEM_DEPLOYED',
+        payload: deployedMillionairesProblem
+    };
+};
+
+// Redux action for when richest millionaire's address has been computed
+export const computeRichestMillionaire = (richestMillionaire) => {
+    return {
+        type: 'RICHEST_MILLIONAIRE_COMPUTED',
+        payload: richestMillionaire
+    };
+};
+
+// Redux action for notification message has been sent for snackbar display
 export const notifyMessage = (notification) => {
     return {
         type: 'MESSAGE_NOTIFIED',
--- client/src/components/App.js
+++ client/src/components/App.js
@@ -10,9 +10,10 @@ import { withStyles } from "@material-ui/core";
 import getEnigmaInit from "../utils/getEnigmaInit.js";
 // Imports - Components
 import Header from "./Header";
+import MillionairesProblem from './MillionairesProblem';
 import "../App.css";
 // Imports - Actions (Redux)
-import { initializeEnigma, initializeAccounts } from '../actions';
+import { initializeEnigma, initializeAccounts, deployMillionairesProblem } from '../actions';
 
 const styles = theme => ({
     root: {
@@ -39,6 +40,11 @@ class App extends Component {
         const accounts = await enigma.web3.eth.getAccounts();
         // Create redux action to initialize set state variable containing unlocked accounts
         this.props.initializeAccounts(accounts);
+        const secretContractCount = await enigma.enigmaContract.methods.countSecretContracts().call();
+        const deployedMillionairesProblemAddress = (await enigma.enigmaContract.methods
+            .getSecretContractAddresses(secretContractCount-1, secretContractCount).call())[0];
+        // Create redux action to set state variable containing deployed millionaires' problem secret contract address
+        this.props.deployMillionairesProblem(deployedMillionairesProblemAddress);
     }
 
     render() {
@@ -52,9 +58,14 @@ class App extends Component {
         }
         else {
             return (
-                <div className="App">
-                    <Header/>
-                    <Message color="green">Enigma setup has loaded!</Message>
+                <div className='App'>
+                    <Header />
+                    <br />
+                    <Container>
+                        <Paper>
+                            <MillionairesProblem />
+                        </Paper>
+                    </Container>
                 </div>
             );
         }
@@ -67,5 +78,5 @@ const mapStateToProps = (state) => {
 
 export default connect(
     mapStateToProps,
-    { initializeEnigma, initializeAccounts }
+    { initializeEnigma, initializeAccounts, deployMillionairesProblem }
 )(withStyles(styles)(App));
