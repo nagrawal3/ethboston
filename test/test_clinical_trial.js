2cc2ff9d23cd3d8344da7ea60774127f6bb961a3
stevenvaleri <steven.valeri@ey.com>
2019-09-08 04:46:13 -0400

    smart contract and full test case

--- test/test_clinical_trial.js
+++ test/test_clinical_trial.js
@@ -74,7 +74,7 @@ contract("ClinicalTrial", accounts => {
       process.stdout.write('Waiting. Current Task Status is '+task.ethStatus+'\r');
     } while (task.ethStatus !== 2);
     expect(task.ethStatus).to.equal(2);
-    process.stdout.write('Completed. Final Task Status is '+task.ethStatus+'\n');
+    process.stdout.write('Participant 1 Added. Final Task Status is '+task.ethStatus+'\n');
   }, 10000);
 
   // ADDING SECOND PARTICIPANT
@@ -109,7 +109,7 @@ contract("ClinicalTrial", accounts => {
       process.stdout.write('Waiting. Current Task Status is '+task.ethStatus+'\r');
     } while (task.ethStatus !== 2);
     expect(task.ethStatus).to.equal(2);
-    process.stdout.write('Completed. Final Task Status is '+task.ethStatus+'\n');
+    process.stdout.write('Participant 2 Added. Final Task Status is '+task.ethStatus+'\n');
   }, 10000);
 
   // ADDING THIRD PARTICIPANT
@@ -144,7 +144,7 @@ contract("ClinicalTrial", accounts => {
       process.stdout.write('Waiting. Current Task Status is '+task.ethStatus+'\r');
     } while (task.ethStatus !== 2);
     expect(task.ethStatus).to.equal(2);
-    process.stdout.write('Completed. Final Task Status is '+task.ethStatus+'\n');
+    process.stdout.write('Participant 3 Added. Final Task Status is '+task.ethStatus+'\n');
   }, 10000);
 
   // ADDING FOURTH PARTICIPANT
@@ -179,15 +179,14 @@ contract("ClinicalTrial", accounts => {
       process.stdout.write('Waiting. Current Task Status is '+task.ethStatus+'\r');
     } while (task.ethStatus !== 2);
     expect(task.ethStatus).to.equal(2);
-    process.stdout.write('Completed. Final Task Status is '+task.ethStatus+'\n');
+    process.stdout.write('Participant 4 Added. Final Task Status is '+task.ethStatus+'\n');
   }, 10000);
-
 
   // GET NUMBER OF PARTICIPANTS
   it('should execute get number of participants', async () => {
       let taskFn = 'get_participant_count()';
       let taskArgs = [];
-      let taskGasLimit = 500000;
+      let taskGasLimit = 6721900;
       let taskGasPx = utils.toGrains(1);
       task = await new Promise((resolve, reject) => {
           enigma.computeTask(taskFn, taskArgs, taskGasLimit, taskGasPx, accounts[0], contractAddr)
@@ -208,7 +207,7 @@ contract("ClinicalTrial", accounts => {
           process.stdout.write('Waiting. Current Task Status is '+task.ethStatus+'\r');
       } while (task.ethStatus !== 2);
       expect(task.ethStatus).to.equal(2);
-      process.stdout.write('Completed. Final Task Status is '+task.ethStatus+'\n');
+      process.stdout.write('Confirmed correct # of participants exist. Final Task Status is '+task.ethStatus+'\n');
   }, 10000);
 
   it('should get the result and verify the computation is correct', async () => {
@@ -222,6 +221,307 @@ contract("ClinicalTrial", accounts => {
     expect(web3.eth.abi.decodeParameters([{
         type: 'uint256',
         name: 'participantCount',
-    }], task.decryptedOutput).participantCount).to.equal('3');
+    }], task.decryptedOutput).participantCount).to.equal('4');
   });
+
+  // SET RESULT 1
+  it('should set results of participant 1', async () => {
+      let taskFn = 'write_result()';
+      let taskArgs = [
+        [1, 'uint256'],
+        [10, 'uint256'],
+      ];
+      let taskGasLimit = 6721900;
+      let taskGasPx = utils.toGrains(1);
+      task = await new Promise((resolve, reject) => {
+          enigma.computeTask(taskFn, taskArgs, taskGasLimit, taskGasPx, accounts[0], contractAddr)
+              .on(eeConstants.SEND_TASK_INPUT_RESULT, (result) => resolve(result))
+              .on(eeConstants.ERROR, (error) => reject(error));
+      });
+  });
+
+
+  it('should get the pending task', async () => {
+      task = await enigma.getTaskRecordStatus(task);
+      expect(task.ethStatus).to.equal(1);
+  });
+
+  it('should get the confirmed task', async () => {
+      do {
+          await sleep(1000);
+          task = await enigma.getTaskRecordStatus(task);
+          process.stdout.write('Waiting. Current Task Status is '+task.ethStatus+'\r');
+      } while (task.ethStatus !== 2);
+      expect(task.ethStatus).to.equal(2);
+      process.stdout.write('Results Posted 1. Final Task Status is ' + task.ethStatus+'\n');
+  }, 10000);
+
+  // SET RESULT 2
+  it('should set results of participant 1', async () => {
+      let taskFn = 'write_result()';
+      let taskArgs = [
+        [2, 'uint256'],
+        [20, 'uint256'],
+      ];
+      let taskGasLimit = 6721900;
+      let taskGasPx = utils.toGrains(1);
+      task = await new Promise((resolve, reject) => {
+          enigma.computeTask(taskFn, taskArgs, taskGasLimit, taskGasPx, accounts[0], contractAddr)
+              .on(eeConstants.SEND_TASK_INPUT_RESULT, (result) => resolve(result))
+              .on(eeConstants.ERROR, (error) => reject(error));
+      });
+  });
+
+
+  it('should get the pending task', async () => {
+      task = await enigma.getTaskRecordStatus(task);
+      expect(task.ethStatus).to.equal(1);
+  });
+
+  it('should get the confirmed task', async () => {
+      do {
+          await sleep(1000);
+          task = await enigma.getTaskRecordStatus(task);
+          process.stdout.write('Waiting. Current Task Status is '+task.ethStatus+'\r');
+      } while (task.ethStatus !== 2);
+      expect(task.ethStatus).to.equal(2);
+      process.stdout.write('Results Posted 2. Final Task Status is ' + task.ethStatus+'\n');
+  }, 10000);
+
+  // SET RESULT 3
+  it('should set results of participant', async () => {
+      let taskFn = 'write_result()';
+      let taskArgs = [
+        [3, 'uint256'],
+        [30, 'uint256'],
+      ];
+      let taskGasLimit = 6721900;
+      let taskGasPx = utils.toGrains(1);
+      task = await new Promise((resolve, reject) => {
+          enigma.computeTask(taskFn, taskArgs, taskGasLimit, taskGasPx, accounts[0], contractAddr)
+              .on(eeConstants.SEND_TASK_INPUT_RESULT, (result) => resolve(result))
+              .on(eeConstants.ERROR, (error) => reject(error));
+      });
+  });
+
+
+  it('should get the pending task', async () => {
+      task = await enigma.getTaskRecordStatus(task);
+      expect(task.ethStatus).to.equal(1);
+  });
+
+  it('should get the confirmed task', async () => {
+      do {
+          await sleep(1000);
+          task = await enigma.getTaskRecordStatus(task);
+          process.stdout.write('Waiting. Current Task Status is '+task.ethStatus+'\r');
+      } while (task.ethStatus !== 2);
+      expect(task.ethStatus).to.equal(2);
+      process.stdout.write('Results Posted 3. Final Task Status is ' + task.ethStatus+'\n');
+  }, 10000);
+
+  // SET RESULT 4
+  it('should set results of participant 1', async () => {
+      let taskFn = 'write_result()';
+      let taskArgs = [
+        [4, 'uint256'],
+        [40, 'uint256'],
+      ];
+      let taskGasLimit = 6721900;
+      let taskGasPx = utils.toGrains(1);
+      task = await new Promise((resolve, reject) => {
+          enigma.computeTask(taskFn, taskArgs, taskGasLimit, taskGasPx, accounts[0], contractAddr)
+              .on(eeConstants.SEND_TASK_INPUT_RESULT, (result) => resolve(result))
+              .on(eeConstants.ERROR, (error) => reject(error));
+      });
+  });
+
+
+  it('should get the pending task', async () => {
+      task = await enigma.getTaskRecordStatus(task);
+      expect(task.ethStatus).to.equal(1);
+  });
+
+  it('should get the confirmed task', async () => {
+      do {
+          await sleep(1000);
+          task = await enigma.getTaskRecordStatus(task);
+          process.stdout.write('Waiting. Current Task Status is '+task.ethStatus+'\r');
+      } while (task.ethStatus !== 2);
+      expect(task.ethStatus).to.equal(2);
+      process.stdout.write('Results Posted 4. Final Task Status is ' + task.ethStatus+'\n');
+  }, 10000);
+
+  // Get the sum of ages
+  it('should execute get the sum of results', async () => {
+      let taskFn = 'sum_ages()';
+      let taskArgs = [];
+      let taskGasLimit = 6721900;
+      let taskGasPx = utils.toGrains(1);
+      task = await new Promise((resolve, reject) => {
+          enigma.computeTask(taskFn, taskArgs, taskGasLimit, taskGasPx, accounts[0], contractAddr)
+              .on(eeConstants.SEND_TASK_INPUT_RESULT, (result) => resolve(result))
+              .on(eeConstants.ERROR, (error) => reject(error));
+      });
+  });
+
+  it('should get the pending task', async () => {
+      task = await enigma.getTaskRecordStatus(task);
+      expect(task.ethStatus).to.equal(1);
+  });
+
+  it('should get the confirmed task', async () => {
+      do {
+          await sleep(1000);
+          task = await enigma.getTaskRecordStatus(task);
+          process.stdout.write('Waiting. Current Task Status is '+task.ethStatus+'\r');
+      } while (task.ethStatus !== 2);
+      expect(task.ethStatus).to.equal(2);
+      process.stdout.write('Sum of ages is correct. Final Task Status is ' + task.ethStatus+'\n');
+  }, 10000);
+
+  it('Sum of ages', async () => {
+    task = await new Promise((resolve, reject) => {
+      enigma.getTaskResult(task)
+        .on(eeConstants.GET_TASK_RESULT_RESULT, (result) => resolve(result))
+        .on(eeConstants.ERROR, (error) => reject(error));
+    });
+    expect(task.engStatus).to.equal('SUCCESS');
+    task = await enigma.decryptTaskResult(task);
+    expect(web3.eth.abi.decodeParameters([{
+        type: 'uint256',
+        name: 'resultSum',
+    }], task.decryptedOutput).resultSum).to.equal('231');
+  });
+
+  // Get the sum of results
+  it('should execute get the sum of results', async () => {
+      let taskFn = 'sum_results()';
+      let taskArgs = [ ];
+      let taskGasLimit = 6721900;
+      let taskGasPx = utils.toGrains(1);
+      task = await new Promise((resolve, reject) => {
+          enigma.computeTask(taskFn, taskArgs, taskGasLimit, taskGasPx, accounts[0], contractAddr)
+              .on(eeConstants.SEND_TASK_INPUT_RESULT, (result) => resolve(result))
+              .on(eeConstants.ERROR, (error) => reject(error));
+      });
+  });
+
+  it('should get the pending task', async () => {
+      task = await enigma.getTaskRecordStatus(task);
+      expect(task.ethStatus).to.equal(1);
+  });
+
+  it('should get the confirmed task', async () => {
+      do {
+          await sleep(1000);
+          task = await enigma.getTaskRecordStatus(task);
+          process.stdout.write('Waiting. Current Task Status is '+task.ethStatus+'\r');
+      } while (task.ethStatus !== 2);
+      expect(task.ethStatus).to.equal(2);
+      process.stdout.write('Sum of ages is correct. Final Task Status is ' + task.ethStatus+'\n');
+  }, 10000);
+
+  it('Sum of ages', async () => {
+    task = await new Promise((resolve, reject) => {
+      enigma.getTaskResult(task)
+        .on(eeConstants.GET_TASK_RESULT_RESULT, (result) => resolve(result))
+        .on(eeConstants.ERROR, (error) => reject(error));
+    });
+    expect(task.engStatus).to.equal('SUCCESS');
+    task = await enigma.decryptTaskResult(task);
+    expect(web3.eth.abi.decodeParameters([{
+        type: 'uint256',
+        name: 'resultSum',
+    }], task.decryptedOutput).resultSum).to.equal('100');
+  });
+
+  // compute average results REAL
+  it('should execute get the average of real results', async () => {
+      let taskFn = 'compute_avg_result(bool)';
+      let taskArgs = [
+        [1, 'bool'],
+      ];
+      let taskGasLimit = 6721900;
+      let taskGasPx = utils.toGrains(1);
+      task = await new Promise((resolve, reject) => {
+          enigma.computeTask(taskFn, taskArgs, taskGasLimit, taskGasPx, accounts[0], contractAddr)
+              .on(eeConstants.SEND_TASK_INPUT_RESULT, (result) => resolve(result))
+              .on(eeConstants.ERROR, (error) => reject(error));
+      });
+  });
+
+  it('should get the pending task', async () => {
+      task = await enigma.getTaskRecordStatus(task);
+      expect(task.ethStatus).to.equal(1);
+  });
+
+  it('should get the confirmed task', async () => {
+      do {
+          await sleep(1000);
+          task = await enigma.getTaskRecordStatus(task);
+          process.stdout.write('Waiting. Current Task Status is '+task.ethStatus+'\r');
+      } while (task.ethStatus !== 2);
+      expect(task.ethStatus).to.equal(2);
+      process.stdout.write('Real average is correct. Final Task Status is ' + task.ethStatus+'\n');
+  }, 10000);
+
+  it('Sum of ages', async () => {
+    task = await new Promise((resolve, reject) => {
+      enigma.getTaskResult(task)
+        .on(eeConstants.GET_TASK_RESULT_RESULT, (result) => resolve(result))
+        .on(eeConstants.ERROR, (error) => reject(error));
+    });
+    expect(task.engStatus).to.equal('SUCCESS');
+    task = await enigma.decryptTaskResult(task);
+    expect(web3.eth.abi.decodeParameters([{
+        type: 'uint256',
+        name: 'resultSum',
+    }], task.decryptedOutput).resultSum).to.equal('20');
+  });
+
+  // compute average results false
+  it('should execute get the average of placebo results', async () => {
+      let taskFn = 'compute_avg_result(bool)';
+      let taskArgs = [
+        [0, 'bool'],
+      ];
+      let taskGasLimit = 6721900;
+      let taskGasPx = utils.toGrains(1);
+      task = await new Promise((resolve, reject) => {
+          enigma.computeTask(taskFn, taskArgs, taskGasLimit, taskGasPx, accounts[0], contractAddr)
+              .on(eeConstants.SEND_TASK_INPUT_RESULT, (result) => resolve(result))
+              .on(eeConstants.ERROR, (error) => reject(error));
+      });
+  });
+
+  it('should get the pending task', async () => {
+      task = await enigma.getTaskRecordStatus(task);
+      expect(task.ethStatus).to.equal(1);
+  });
+
+  it('should get the confirmed task', async () => {
+      do {
+          await sleep(1000);
+          task = await enigma.getTaskRecordStatus(task);
+          process.stdout.write('Waiting. Current Task Status is '+task.ethStatus+'\r');
+      } while (task.ethStatus !== 2);
+      expect(task.ethStatus).to.equal(2);
+      process.stdout.write('Placebo average is correct. Final Task Status is ' + task.ethStatus+'\n');
+  }, 10000);
+
+  it('Sum of ages', async () => {
+    task = await new Promise((resolve, reject) => {
+      enigma.getTaskResult(task)
+        .on(eeConstants.GET_TASK_RESULT_RESULT, (result) => resolve(result))
+        .on(eeConstants.ERROR, (error) => reject(error));
+    });
+    expect(task.engStatus).to.equal('SUCCESS');
+    task = await enigma.decryptTaskResult(task);
+    expect(web3.eth.abi.decodeParameters([{
+        type: 'uint256',
+        name: 'resultSum',
+    }], task.decryptedOutput).resultSum).to.equal('30');
+  });
+
 });
